008d0b5b168054cb2026b55513486b088580817a
kesc <492167585@qq.com>
2020-06-05 11:38:59 +0800

    调整默认线程池大小；优化线程池创建方法；增加submit方法

--- src/main/java/com/xjbg/java/sdk/util/ThreadPoolUtils.java
+++ src/main/java/com/xjbg/java/sdk/util/ThreadPoolUtils.java
@@ -7,39 +7,59 @@ import java.util.concurrent.*;
  * @time: 2017-12-19 20:05
  */
 public class ThreadPoolUtils {
-    private static final ThreadPoolExecutor POOL = create(5);
-    private static final ScheduledThreadPoolExecutor SCHEDULED_POOL = createScheduledPool(5);
+    private static final ThreadPoolExecutor POOL = create(8);
+    private static final ScheduledThreadPoolExecutor SCHEDULED_POOL = createScheduledPool(8);
 
-    /**
-     * @param size
-     * @return
-     */
     public static ThreadPoolExecutor create(int size) {
-        return new ThreadPoolExecutor(Math.min(size, Runtime.getRuntime().availableProcessors()),
-                Math.max(size * 2, Runtime.getRuntime().availableProcessors()),
-                120L, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(1000),
-                new ThreadPoolExecutor.CallerRunsPolicy());
+        return create(size, 1000);
     }
 
-    /**
-     * @param size
-     * @return
-     */
     public static ThreadPoolExecutor create(int size, int queueSize) {
+        return create(size, queueSize, Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+    public static ThreadPoolExecutor create(int size,
+                                            int queueSize,
+                                            RejectedExecutionHandler rejectedExecutionHandler) {
+        return create(size, queueSize, Executors.defaultThreadFactory(), rejectedExecutionHandler);
+    }
+
+    public static ThreadPoolExecutor create(int size,
+                                            int queueSize,
+                                            ThreadFactory threadFactory,
+                                            RejectedExecutionHandler rejectedExecutionHandler) {
         return new ThreadPoolExecutor(Math.min(size, Runtime.getRuntime().availableProcessors()),
                 Math.max(size * 2, Runtime.getRuntime().availableProcessors()),
                 120L, TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(queueSize),
-                new ThreadPoolExecutor.CallerRunsPolicy());
+                threadFactory,
+                rejectedExecutionHandler);
     }
 
     public static ScheduledThreadPoolExecutor createScheduledPool(int size) {
-        return new ScheduledThreadPoolExecutor(size, new ThreadPoolExecutor.CallerRunsPolicy());
+        return createScheduledPool(size, new ThreadPoolExecutor.CallerRunsPolicy());
+    }
+
+    public static ScheduledThreadPoolExecutor createScheduledPool(int size, RejectedExecutionHandler rejectedExecutionHandler) {
+        return createScheduledPool(size, Executors.defaultThreadFactory(), rejectedExecutionHandler);
+    }
+
+    public static ScheduledThreadPoolExecutor createScheduledPool(int size,
+                                                                  ThreadFactory threadFactory,
+                                                                  RejectedExecutionHandler rejectedExecutionHandler) {
+        return new ScheduledThreadPoolExecutor(size, threadFactory, rejectedExecutionHandler);
     }
 
     public static void execute(Runnable runnable) {
         POOL.execute(runnable);
+    }
+
+    public static Future<?> submit(Runnable runnable) {
+        return POOL.submit(runnable);
+    }
+
+    public static Future<?> submitSchedule(Runnable runnable) {
+        return SCHEDULED_POOL.submit(runnable);
     }
 
     public static ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
