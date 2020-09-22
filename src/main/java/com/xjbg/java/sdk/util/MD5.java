079a638b6c8b8e794c1c2f708de81756d5d976fc
kesc <492167585@qq.com>
2020-09-22 17:02:12 +0800

    优化md5工具

--- src/main/java/com/xjbg/java/sdk/util/MD5.java
+++ src/main/java/com/xjbg/java/sdk/util/MD5.java
@@ -1,10 +1,10 @@
 package com.xjbg.java.sdk.util;
 
-import com.xjbg.java.sdk.enums.Encoding;
+import lombok.SneakyThrows;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * @author keshicong
@@ -13,8 +13,11 @@ import java.security.NoSuchAlgorithmException;
  * @date 2016年10月1日 下午2:58:49
  */
 public class MD5 {
+    /**
+     * 用来将字节转换成 16 进制表示的字符
+     */
+    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private static volatile MD5 instance = null;
-    private final String charset = Encoding.UTF_8.getEncoding();
 
     private MD5() {
         super();
@@ -31,54 +34,61 @@ public class MD5 {
         return instance;
     }
 
-    public String getMD5(final byte[] original) throws NoSuchAlgorithmException {
-        if (original == null) {
-            return null;
-        }
-        // 用来将字节转换成 16 进制表示的字符
-        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(original);
-        // MD5 的计算结果是一个 128 位的长整数 用字节表示就是 16 个字节
-        byte tmp[] = md.digest();
-        char str[] = new char[16 * 2];
+    private String toHex(byte[] input) {
+        char[] str = new char[16 * 2];
         byte temp;
         int k = 0;
         for (int i = 0; i < 16; i++) {
-            temp = tmp[i];
+            temp = input[i];
             // 取字节中高 4 位的数字转换 >>>为逻辑右移，将符号位一起右移
-            str[k++] = hexDigits[temp >>> 4 & 0xf];
+            str[k++] = HEX_DIGITS[temp >>> 4 & 0xf];
             // 取字节中低 4位的数字转换
-            str[k++] = hexDigits[temp & 0xf];
+            str[k++] = HEX_DIGITS[temp & 0xf];
         }
         return new String(str);
     }
 
-    /**
-     * @param original
-     * @return
-     * @throws UnsupportedEncodingException
-     * @throws NoSuchAlgorithmException
-     * @Description get md5 of input string
-     */
-    public String getMD5(final String original, final String charset) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    @SneakyThrows
+    public byte[] md5(final byte[] original) {
         if (original == null) {
             return null;
         }
-        return getMD5(original.getBytes(charset));
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(original);
+        // MD5 的计算结果是一个 128 位的长整数 用字节表示就是 16 个字节
+        return md.digest();
     }
 
-    /**
-     * @param original
-     * @return
-     * @throws UnsupportedEncodingException
-     * @throws NoSuchAlgorithmException
-     * @Description get md5 of input string
-     */
-    public String getMD5(final String original) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    public byte[] md5(final String original, final Charset charset) {
         if (original == null) {
             return null;
         }
-        return getMD5(original.getBytes(charset));
+        return md5(original.getBytes(charset));
+    }
+
+    public byte[] md5(final String original) {
+        if (original == null) {
+            return null;
+        }
+        return md5(original.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String md5hex(final byte[] original) {
+        return toHex(md5(original));
+    }
+
+
+    public String md5hex(final String original, final Charset charset) {
+        if (original == null) {
+            return null;
+        }
+        return md5hex(original.getBytes(charset));
+    }
+
+    public String md5hex(final String original) {
+        if (original == null) {
+            return null;
+        }
+        return md5hex(original.getBytes(StandardCharsets.UTF_8));
     }
 }
